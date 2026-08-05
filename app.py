65d7d9cc76b9d97f04f735c1c711386967505d4c
blinking-led <blinking-led@users.noreply.github.com>
2026-08-05 13:24:58 +0300

    Update app.py to version v2

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
+request_count = 0
 
 @app.route('/')
 def hello():
-	return{
-		"service":"cicd-demo",
-		"version": "1.0",
-		"message": "Привет! Версия 1.0"
-	}
-	
+    global request_count
+    request_count += 1
+    return {
+        "service": "cicd-demo",
+        "version": "2.0",
+        "message": "Привет! Это сервис v2.0",
+        "timestamp": datetime.now().isoformat(),
+        "request_count": request_count
+    }
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
